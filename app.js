dc0894ba44db5d4f03c88b30e77ea99c92a85918
core.quotepath <takuya.a.1004@gmail.com>
2018-09-16 00:41:35 +0900

    basic constitution

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+const path = require('path');
+const routes = require('./routes/index');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(expressLayouts);
+app.use('/', routes);
 
 app.listen(3000, function () {
   console.log('listening on port 3000');

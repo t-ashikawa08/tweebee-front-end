fa95d08735c8e17e53a3f4d37f3fea505bae780c
core.quotepath <takuya.a.1004@gmail.com>
2018-09-16 03:46:38 +0900

    index add

--- app.js
+++ app.js
@@ -13,6 +13,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(expressLayouts);
 app.use('/', routes);
 
+app.use(function (req, res, next) {
+  res.removeHeader('X-Powered-By');
+  res.removeHeader('ETag');
+  res.header('Cache-Control', ['private', 'no-store', 'no-cache', 'must-revalidate', 'proxy-revalidate'].join(','));
+  res.header('no-cache', 'Set-Cookie');
+});
+
 app.listen(3000, function () {
   console.log('listening on port 3000');
 });
